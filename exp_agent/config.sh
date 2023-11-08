e1d06011c4e0096fd77d9e25f6a01d1862f1bbc3
ShibuyaMasataka <masa.home.shibuya@gmail.com>
2023-11-08 22:10:58 +0900

    sample

---
+++ exp_agent/config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+GITHUB_OWNNER="ShibuyaMasataka"
+GITHUB_REPO="kansai-kaggler-autumn-2023"
+WANDB_SCORE_NAME="cv"
+SCORE_DIRECTION="minimize"
+EXP_CODE_PATH="../run.py"
+LLM_MODEL="gpt-4"
